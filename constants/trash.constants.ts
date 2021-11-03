e219b5cc19c7ab0a44dca32b242d338056bd869e
Tiago Ribeiro <tiago.err@outlook.com>
2021-11-03 10:05:27 +0000

    Added the trash labels in portuguese as well

--- constants/trash.constants.ts
+++ constants/trash.constants.ts
@@ -2,26 +2,32 @@ export const TRASH_DAYS = [
 	undefined,
 	{
 		label: "Metal & Plastic",
+		label_pt: "Plástico & Metal",
 		color: "#FFCC2F",
 	},
 	{
 		label: "Organic waste",
+		label_pt: "Resíduos Orgânicos",
 		color: "#a34832",
 	},
 	{
 		label: "Other waste",
+		label_pt: "Resíduos Indiferenciados",
 		color: "#7a7c7f",
 	},
 	{
 		label: "Paper",
+		label_pt: "Papel & Cartão",
 		color: "#2075BC",
 	},
 	{
 		label: "Organic waste",
+		label_pt: "Resíduos Orgânicos",
 		color: "#a34832",
 	},
 	{
 		label: "Glass",
+		label_pt: "Vidro",
 		color: "#10a84f",
 	},
 ];
